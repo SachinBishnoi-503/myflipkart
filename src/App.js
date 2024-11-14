d24561a0aed94a3ccdd2058b76502f8294df1ee9
SachinBishnoi-503 <bishnoisachin626@gmail.com>
2024-11-14 23:09:13 +0530

    error coming

--- src/App.js
+++ src/App.js
@@ -5,17 +5,20 @@ import HomePage from './components/HomePage';
 import BuyNowForm from './components/common/BuyNowForm';
 import OrderSummary from './components/common/OrderSummary';
 import Payment from './components/common/Payment';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 function App() {
   return (
     <div className='max-w-[480px] mx-auto'>
 
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/product/:id" element={<ProductDetail />} />
-        <Route path="/buy-now/:id" element={<BuyNowForm />} />
-        <Route path="/OrderSummary" element={<OrderSummary />} />
-        <Route path="/Payment" element={<Payment />} />
-      </Routes>
+            <Routes>
+                <Route path="/" element={<HomePage />} />
+                <Route path="/product/:id" element={<ProductDetail />} />
+                <Route path="/buy-now/:id" element={<BuyNowForm />} />
+                <Route path="/OrderSummary" element={<OrderSummary />} />
+                <Route path="/Payment" element={<Payment />} />
+            </Routes>
     </div>
   );
 }
