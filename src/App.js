5157ec42167633f8a20f1fcefafe40b81693d43e
ajay-bishnoi <AJAY2003BISHNOI@GMAIL.COM>
2024-11-15 21:18:37 +0530

    error solve

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import { Route, Routes } from 'react-router-dom';
-import './App.css';
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
 import ProductDetail from "./components/phoneDetail/ProductDetail";
-import HomePage from './components/HomePage';
-import BuyNowForm from './components/common/BuyNowForm';
-import OrderSummary from './components/common/OrderSummary';
-import Payment from './components/common/Payment';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import HomePage from "./components/HomePage";
+import BuyNowForm from "./components/common/BuyNowForm";
+import OrderSummary from "./components/common/OrderSummary";
+import Payment from "./components/common/Payment";
+
 function App() {
   return (
-    <div className='max-w-[480px] mx-auto'>
-
-            <Routes>
-                <Route path="/" element={<HomePage />} />
-                <Route path="/product/:id" element={<ProductDetail />} />
-                <Route path="/buy-now/:id" element={<BuyNowForm />} />
-                <Route path="/OrderSummary" element={<OrderSummary />} />
-                <Route path="/Payment" element={<Payment />} />
-            </Routes>
+    <div className="max-w-[480px] mx-auto">
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/product/:id" element={<ProductDetail />} />
+        <Route path="/buy-now/:id" element={<BuyNowForm />} />
+        <Route path="/OrderSummary" element={<OrderSummary />} />
+        <Route path="/Payment" element={<Payment />} />
+      </Routes>
     </div>
   );
 }
