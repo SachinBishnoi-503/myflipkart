d297b7d2e31a6c146e8028f4a2523c5be7b34686
naman-398 <you@example.com>
2024-11-16 23:39:28 +0530

    done

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,21 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      boxShadow: {
+        "login": "0px 0px 25px 0px #0303031F"
+      },
+      fontFamily: {
+        "poppins": '"Poppins", system-ui',
+        "inter": '"Inter", system-ui'
+      },
+      colors: {
+        "off-white": "#FEFEFE",
+        "rich-black": "#030303",
+        "palatinate-blue": "#2A55E5",
+        "amber": "#FFC200"
+      }
+    },
   },
   plugins: [],
 }
